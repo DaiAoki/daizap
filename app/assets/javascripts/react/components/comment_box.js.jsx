41d6561e9009ada21bb52f81baef3fed7aa3ed84
aokidai <aokidai@Dais-MacBook-Air.local>
2017-11-14 23:56:58 +0900

    react playground.

--- app/assets/javascripts/react/components/comment_box.js.jsx
+++ app/assets/javascripts/react/components/comment_box.js.jsx
@@ -1,9 +1,30 @@
 var CommentBox = createReactClass({
-
   render: function() {
     return (
-      <div class="react-sample">
-        playground
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
+var CommentList = createReactClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+});
+
+var CommentForm = createReactClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
       </div>
     );
   }
