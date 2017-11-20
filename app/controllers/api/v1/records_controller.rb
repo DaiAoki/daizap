c514e6aaed8af86c970f6c95a5ef089cc18dd3ba
aokidai <aokidai@Dais-MacBook-Air.local>
2017-11-21 00:42:24 +0900

    implement destroy.

--- app/controllers/api/v1/records_controller.rb
+++ app/controllers/api/v1/records_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::RecordsController < ApplicationController
+  before_action :reject_unless_record_user, only: :destroy
 
   def index
     records = Record.all
@@ -14,11 +15,23 @@ class Api::V1::RecordsController < ApplicationController
     end
   end
 
+  def destroy
+    if Record.find(params[:id]).destroy
+      flash[:success] = 'Success!'
+    else
+      flash[:warning] = 'Failure!'
+    end
+  end
+
 
 
   private
 
     def record_params
       params.permit(:weight, :image)
+    end
+
+    def reject_unless_record_user
+      return unless Record.find(params[:id]).user == current_user
     end
 end
--- config/routes/api/routes.rb
+++ config/routes/api/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api, format: 'json' do
     namespace :v1 do
       resources :comments, only: [:index, :create]
-      resources :records, only: [:index, :create]
+      resources :records, only: [:index, :create, :destroy]
     end
   end
 end
